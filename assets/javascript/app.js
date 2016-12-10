219c34094366d94f278cbe13be6f202bfd558783
CMarie <KickAss@Christas-MacBook-Pro.local>
2016-12-10 13:17:01 -0500

    printing to console, help from nuno

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,82 +17,30 @@ var trivia = [
 	
 	{
         question: "What's in a Vodka Martini?",
-        answers: {
-            1: "gin, sweet vermouth",
-            2: "vodka, soda",
-            3: "vodka, dry vermouth",
-            4: "vodka, sweet vermouth"
-        },
+        answers: ['gin, sweet vermouth','vodka, soda', 'vodka, dry vermouth','vodka, sweet vermouth'],
         rightAnswer: 3
     },
 
     {
-    	question: "What's in a Manhattan?",
-    	answers: {
-    		1: "whiskey, grenadine",
-    		2: "whiskey, sweet vermouth",
-    		3: "scotch, water",
-    		4: "scotch, soda"
-    	}, 
-    	rightAnswer: 2
-    }
+    	 question: "What's in a Manhattan",
+        answers: ['whiskey, water','whiskey, soda', 'whiskey, dry vermouth','whiskey, sweet vermouth'],
+        rightAnswer: 4
 
-]
+    }];
 
-$(document).on("click", "#start", function showQuestion (id){
-	$("#displayQuestion").empty();	
+$(document).on("click", function (){
+	for (var i = 0; i < trivia.length; i++) {
+		console.log(trivia[i].question);
+		$("#displayQuestion").html("<h2>" + trivia[i].question + "</h2>")
 
-	$.each(trivia, function (key, value) {
-
-	    $("#displayQuestion").text(key + ": " + value.question);
-
-
-	for (var i = 0; i < value.answers.length; i++)   
-	    $.each(value.answers, function (key, value) {
-
-	        $('<button>');
-	        	$('#displayAnswers').text(value);
-
-	    	});
-		});    
-});
-
-// 	for (var i = 0; i < trivia; i++) 
-// 		$("#displayQuestion").text(trivia[i].question);
-
-// });
-// var random = Math.floor(Math.random) * (10-1) + 1; 
-// 		for (var i = 0; i < trivia.length; i++);
-// 	$("#answers").empty();
-	 
-// 	// for each answer
-
-// 	// append value to #answers
-
-// 	// make it look like this
-
-// 	// <li class="answerthequestion" data-rightanswer="1">name</li>
-// }
-
-//  $(document).on("click", "#start", function (){
-
-//  	console.log(" The current question is " + currentQuestion)
-//  	if (trivia.length == currentQuestion){
-//  		alert("Game Over");
-//  	}else{
-
-
-//  		showQuestion(currentQuestion);
-//  		currentQuestion = currentQuestion + 1; 
-//  	}
- 	
- // });
-// 	var question = Math.floor(Math.random() * (10 - 1) + 1) + 1;
-// 	// var question = Math.floor(Math.random() * 1) + 1;
-// 		for (var i = 0; i < trivia.length; i++){
-// 			$(".displayQuestion").append(trivia[i].question);
-// 			console.log();
-// 	}	
-// }
-
+		for (var j = 0; j < trivia[i].answers.length; j++){
+	
+			var answers = trivia[i].answers[j]
+			console.log(answers);
+			var buttons = $("<button>").text(answers).attr('data-val', j);
+			$("displayAnswers").append(buttons);
+		}
+		
+	};
+}); 
 
