88d30474f4c4171f0fbe096d65e5f2f05fae6da9
CMarie <KickAss@Christas-MacBook-Pro.local>
2016-12-12 19:47:36 -0500

    one question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,41 +6,99 @@
 //prompts next question - 
 
 //win page / lose page? tally page?
-var counter = 0;
-var score = 0;
-var userChoice = ""; 
-var correctAnswer;
-var currentQuestion = 0; 
+// var counter = 0;
+// var score = 0;
+// var userChoice = ""; 
+// var correctAnswer;
+// var currentQuestion = 0; 
 
 
 var trivia = [
 	
 	{
         question: "What's in a Vodka Martini?",
-        answers: ['gin, sweet vermouth','vodka, soda', 'vodka, dry vermouth','vodka, sweet vermouth'],
+        answers: ['gin, sweet vermouth','vodka, soda', 'vodka, dry vermouth','vodka, sweet vermouth'], 
         rightAnswer: 3
+    	
     },
 
     {
-    	 question: "What's in a Manhattan",
+    	question: "What's in a Manhattan?",
         answers: ['whiskey, water','whiskey, soda', 'whiskey, dry vermouth','whiskey, sweet vermouth'],
         rightAnswer: 4
+    },
 
-    }];
+    {
+    	question: "What's in a Rusty Nail?",
+		answers: ['scotch, drambuie', 'irish creme, whiskey', 'scotch, amaretto','coffee liquor, dark rum'],
+        rightAnswer: 1
+    },
 
+
+];
+
+// var questions = trivia[i].question;
+// var choices = trivia[0].question.answers;
+
+//on start, display first question
 $(document).on("click", function (){
-	for (var i = 0; i < trivia.length; i++) {
-		console.log(trivia[i].question);
-		$("#displayQuestion").html("<h2>" + trivia[i].question + "</h2>")
 
-		for (var j = 0; j < trivia[i].answers.length; j++){
-	
-			var answers = trivia[i].answers[j]
-			console.log(answers);
-			var buttons = $("<button>").text(answers).attr('data-val', j);
-			$("displayAnswers").append(buttons);
-		}
+	//run through length of trivia to print selected question
+	for (var i = 0; i < trivia.length; i++) {
+
+		//empty div
+		$("#displayQuestion").empty();
+
+		var question = trivia[i].question;
 		
-	};
-}); 
+
+		//print to html
+		$("#displayQuestion").html("<h2>" + question + "</h2>")
+ 
+
+	//assigns variable to find answers quicker 	
+	var choices = trivia[i].answers; 
+		
+	//for loops over trivia.answers
+	for (var j = 0; j < choices.length; j++){
+
+		//empty div
+		$("#displayAnswers").empty();
+
+		//creates button per choice
+		var buttons = $('<button>').text(choices[j]).attr('class', 'btn').attr('data-value', choices[j].split);
+		
+
+		//appends buttons to displayAnswers div 
+		$("#displayAnswers").append(buttons);
+		}	
+	}
+	
+});
+// }); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
